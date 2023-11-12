9a0dd6a618a24992b16808e4976d3738895adec6
ManHingHUI <xuwenxing00@gmail.com>
2023-11-12 16:17:51 +0800

    test complete

--- module1/src/Main.java
+++ module1/src/Main.java
@@ -3,6 +3,8 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+        int i = scanner.nextInt();
+        System.out.println(i);
     }
 }
 //cmd + o   搜索類查看文檔
